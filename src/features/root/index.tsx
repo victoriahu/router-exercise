b6be245c403f5e842077fd0db0104f0fa9483716
Brian Tedder <btedder@twilio.com>
2023-01-30 12:47:11 -0800

    add clearer use of useLocation

--- src/features/root/index.tsx
+++ src/features/root/index.tsx
@@ -1,9 +1,12 @@
-import { Outlet } from '~/lib/router'
+import { Outlet, useLocation } from '~/lib/router'
 
 export function Root() {
+  const location = useLocation()
+
   return (
     <div>
       <h1>This is the root</h1>
+      <p>Location.pathname is: {location.pathname}</p>
       <Outlet />
     </div>
   )
